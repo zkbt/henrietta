397d1d9f85f600eab66fab18c8df7ea483db8359
Zach Berta-Thompson <zach.bertathompson@colorado.edu>
2018-09-04 18:10:30 -0600

    make wrapper return light curves (instead of just plot)

--- henrietta/lightcurves.py
+++ henrietta/lightcurves.py
@@ -1,16 +1,37 @@
 
-def get_lightcurve(star='Kepler-186', quarter=1, kind='PDCSAP_FLUX'):
+def download_kepler_lc(star='Kepler-186', quarter=1, kind='PDCSAP_FLUX'):
     '''
     This function is a wrapper to download a single quarter of Kepler
-    lightcurve and extract the PDCSAP_FLUX from it.
+    lightcurve data, and extract a LightCurve object from it.
+    
+    When downloading a Kepler LightCurveFile from online, we get lots
+    of information, including multiple different kinds of light curves
+    for the same star. The goal of this little wrapper is to make it 
+    easy to get just one single light curve to play with.
+    
+    Parameters
+    ----------
+    
+    star : str, int
+        The name of the star whose light curve you want to download.
+        This can be a name ("Kepler-42", "KOI-961") or a KIC number
+        (8561063)
+        
+    quarter : int
+        Which Kepler quarter to download?
+    
+    kind : str
+        "SAP_FLUX" = simple aperture photometry
+        "PDCSAP_FLUX" = SAP, with pre-search data conditioning applied,
+                        to get rid of some instrumental systematics 
+                        from the light curves (but watch out! some
+                        astrophysical signals might be messed up too!)
+    
     '''
 
-    (maybe it looks something like this?)
-    lcf = lightkurve.KeplerLightCurveFile.from_archive(star)
     lcf = lightkurve.KeplerLightCurveFile.from_archive(star, quarter=quarter)
-    lcf.plot()
     lc = lcf.get_lightcurve(kind)
-    lc.plot()
+    return lc
 
 def get_all_lightcurves(star='Kepler-42'):
     '''
