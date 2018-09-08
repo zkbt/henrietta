d2766ff2ad54f55c2cf07f66d2f2bd09d9565f0b
Zach Berta-Thompson <zkbt@mit.edu>
2018-09-08 10:59:40 -0600

    add a fake light curve maker

---
+++ henrietta/statistics.py
@@ -0,0 +1,17 @@
+import numpy as np, matplotlib.pyplot as plt
+from scipy.stats import poisson, norm
+from lightkurve import LightCurve
+
+
+
+def create_photon_lightcurve(N=1):
+
+    cadence = 1.0/60/60/24
+    duration = 1.0
+    t = np.arange(0, duration, step=cadence)
+    photons = poisson.rvs(N, size=t.shape)
+    lc = LightCurve(time=t,
+                    flux=photons,
+                    flux_err=np.sqrt(photons),
+                    meta=dict(name='Poisson light curve with {} photons/s'.format(N)))
+    return lc
