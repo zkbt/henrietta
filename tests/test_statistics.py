3941ea48a43aa6b6dbf57f5650da030775a551ec
Zach Berta-Thompson <zkbt@mit.edu>
2018-09-09 22:02:51 -0600

    add tests

---
+++ tests/test_statistics.py
@@ -0,0 +1,16 @@
+from henrietta import *
+
+def test_photons():
+    '''
+    This function tests the toy photon light curve.
+    '''
+
+    lc = create_photon_lightcurve()
+    lc.plot(marker='.', linewidth=0)
+
+def test_bucket():
+    '''
+    This function tests the catch_photons_in_bucket function.
+    '''
+    N = catch_photons_in_bucket(rate=1, time=2, diameter=3, visualize=False)
+    print(N)
