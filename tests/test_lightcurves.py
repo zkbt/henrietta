c5de93555c38942cf92d9a5f16ab3c087f78f108
Zach Berta-Thompson <zach.bertathompson@colorado.edu>
2018-09-12 17:25:06 -0600

    add a test for locate

--- tests/test_lightcurves.py
+++ tests/test_lightcurves.py
@@ -10,3 +10,12 @@ def test_download_kepler_lc():
     lc.plot(marker='.', linewidth=0)
     folded = lc.fold(1.48571092)
     folded.plot(marker='.', linewidth=0)
+
+def test_locate():
+    '''
+    This function should draw a single circle
+    around every transit in the Kepler-17 light curve.
+    '''
+    lc = download_kepler_lc('Kepler-17')
+    lc.plot(marker='.', linewidth=0)
+    locate_transits(lc, period=1.4857108, epoch=2455185.678035)
