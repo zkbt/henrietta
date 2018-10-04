b0de3bdb7bbb1c01034bb4d67fbef5d6c17665b2
Jessica Roberts <jessica@cu-engr2-1-16-10.201.61.10.int.colorado.edu>
2018-10-04 10:57:09 -0600

    Updated with background aperture photometry

--- henrietta/photometry.py
+++ henrietta/photometry.py
@@ -3,8 +3,9 @@ import matplotlib.pyplot as plt
 import photutils
 import pdb
 from phot import *
+plt.ion()
 
-def photometry(ax,image,pos,ap_size=10):
+def photometry(ax,image,pos,ap_size=10,r_in=20,r_out=30,back_photo=True):
 
     '''
     Create apertures around specified list of stars
@@ -20,10 +21,20 @@ def photometry(ax,image,pos,ap_size=10):
         Needs to be as [[0,0],[1,1]...]
     ap_size : int
         The radius of a circular aperture in pixel size
+    r_in : int
+        The inner radius of background aperture in pixel size
+        (Ignore if back_photo=False)
+    r_out : int
+        The outer radius of background aperture in pixel size
+        (Ignore if back_photo=False)
+    back_photo : bool
+        Set to True if want to return an array of background values, False
+        to ignore anything to do with background
 
     Returns
     -------
     photometry of each star : array
+    average background pixel value around each star : array (If back_photo=True)
     plots image with the aperture and centroids located for each star
     '''
 
@@ -31,9 +42,12 @@ def photometry(ax,image,pos,ap_size=10):
     name_stars = []
     for i in nstars:
         name_stars.append('Star {}'.format(i))
-    print name_stars
+    #print name_stars
 
     aperture = photutils.CircularAperture(pos,r=ap_size)
+
+    if back_photo == True:
+        back_aperture = photutils.CircularAnnulus(pos,r_in,r_out)
 
     pos = np.array(pos)
 
@@ -45,12 +59,17 @@ def photometry(ax,image,pos,ap_size=10):
         ax.add_artist(circle1)
         plt.axhline(pos[i,1],xmin=pos[i,0]/100.-.01,xmax=pos[i,0]/100.+.02,color='black')
         plt.axvline(pos[i,0],ymin=pos[i,1]/100.-.01,ymax=pos[i,1]/100.+.02,color='black')
-        plt.text(pos[i,0]+ap_size+1, pos[i,1], name_stars[i])
-        print pos[i,1]
-        print pos[i,0]
+        plt.text(pos[i,0]+ap_size+1, pos[i,1], name_stars[i],zorder=11)
+        #print pos[i,1]
+        #print pos[i,0]
 
+        if back_photo == True:
+            circle2 = plt.Circle((pos[i,0], pos[i,1]), r_in, color='cyan',fill=False,zorder=10)
+            circle3 = plt.Circle((pos[i,0], pos[i,1]), r_out, color='cyan',fill=False,zorder=10)
+            ax.add_artist(circle2)
+            ax.add_artist(circle3)
     #aperture.plot(origin=(0,0),indices=None,ax=ax,fill=False)
-    plt.show()
+    #plt.ion()
 
 
 
@@ -58,4 +77,12 @@ def photometry(ax,image,pos,ap_size=10):
     phot_table = photutils.aperture_photometry(image,aperture)
     flux_values = phot_table['aperture_sum'].data #gets a list of the total flux in specified aperture for each star
 
-    return flux_values
+    if back_photo == True:
+        back_table = photutils.aperture_photometry(image,back_aperture)
+        area = np.pi*(r_out**2-r_in**2)
+        a = np.ones((np.shape(image)[0],np.shape(image)[1]))
+        area = photutils.aperture_photometry(a,back_aperture)
+        back_values = back_table['aperture_sum'].data/area['aperture_sum'].data
+        return flux_values,back_values*np.pi*ap_size**2
+    else:
+        return flux_values
--- henrietta/tests/test_photometry.py
+++ henrietta/tests/test_photometry.py
@@ -4,6 +4,6 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 ax = plt.subplot(111)
-im = create_test_array(N=1, xsize=100, ysize=100, nstars=1, single=True,seed=None)
+im = create_test_array(N=1, xsize=100, ysize=100, nstars=10, single=False,seed=7)
 
-test = photometry(ax,im[0],[[50,40]],ap_size=5)
+test = photometry(ax,im[0],[[54,91],[44,38]],ap_size=3,r_in=8,r_out=12,back_photo=True)
