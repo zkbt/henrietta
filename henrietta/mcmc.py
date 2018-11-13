2a0f80f30bc7e134f9301c7ab23a02fd2590d367
William Waalkes <waalkesw@umich.edu>
2018-11-13 16:18:48 -0700

    MCMC is now fully working

--- henrietta/mcmc.py
+++ henrietta/mcmc.py
@@ -2,6 +2,7 @@ from astropy.modeling import models, fitting, optimizers, statistic, custom_mode
 import numpy as np
 import emcee
 import batman
+import sys
 
 """
 Here I will write a function that takes in an astropy transit model and
@@ -11,16 +12,15 @@ which are variable, and runs an MCMC with those parameters.
 The astropy model already knows which parameters are fixed vs. variable,
 I just need to figure out how to extract those parameters and set up an mcmc
 which will adapt to however many free parameters are presentself.
-
-FOLD and NORMALIZE your lightkurve before running this MCMC
 """
 
 def lnprob(params):
 
     n = len(astropy_model.param_names)
 
+
     i = 0
-    for k in astropy_model.fixed:
+    for k in astropy_model.param_names:
         if astropy_model.fixed[k] == False:
             for m in range(n):
                 if astropy_model.param_names[m] == str(k):
@@ -29,12 +29,12 @@ def lnprob(params):
 
     if (astropy_model.a <= astropy_model.b):
         astropy_model.a = astropy_model.b + 0.01
-        
+
     model = astropy_model(lc.time)
 
     # This is a Gaussian likelihood, for independent data points
 
-    if (0.0 <= astropy_model.radius <= 1.0) and (times[0] <= astropy_model.t0 <= times[-1] ) and (1.0 <= astropy_model.a <= 200.0 ):
+    if (0.0 <= astropy_model.radius <= 1.0) and (lc.time[0] <= astropy_model.t0 <= lc.time[-1] ) and (1.0 <= astropy_model.a <= 200.0 ):
         chisq = np.nansum((lc.flux - model)**2/(lc.flux_err)**2)
         lnp = np.nansum(1/np.sqrt(2*np.pi*(lc.flux_err))) - 0.5*chisq
 
@@ -44,6 +44,18 @@ def lnprob(params):
 
 def mcmc_fit(astropy_model, lc):
 
+    fold_check = input(["Is your light curve folded? (y/n)"])
+
+    if fold_check == "n":
+        lc = lc.fold(period = astropy_model.period.value)
+        print("That's okay, I did it for you")
+
+    norm_check = input(["Is your light curve normalized? (y/n)"])
+
+    if norm_check_check == "n":
+        lc = lc.normalize()
+        print("Do I have to do everything?")
+
     organized = dict(period = astropy_model.period, t0 = astropy_model.t0,
                  radius = astropy_model.radius, a = astropy_model.a,
                  b = astropy_model.b, baseline = astropy_model.baseline,
@@ -51,17 +63,16 @@ def mcmc_fit(astropy_model, lc):
 
     variable_names = []
     i = 0
-    for k in astropy_model.fixed:
+    for k in astropy_model.param_names:
         if astropy_model.fixed[k] == False:
             variable_names.append(str(k))
             i += 1
 
-    # intialize some walkers
-    ndim, nwalkers, nsteps = i, 100, 5000
+    ndim, nwalkers, nsteps = i, 100, 10000
     burnin = int(0.2*nsteps)
 
     param_initial = []
-    for k in organized:
+    for k in astropy_model.param_names:
         if organized[k].fixed == False:
             bounds = organized[k].bounds
             organized[k].initialwalkers = np.random.uniform(bounds[0],bounds[1],nwalkers)
@@ -69,7 +80,6 @@ def mcmc_fit(astropy_model, lc):
 
     p0 = np.transpose(param_initial)
 
-    # create a sampler and run it
     sampler = emcee.EnsembleSampler(nwalkers, ndim, lnprob)
     result = sampler.run_mcmc(p0, nsteps)
 
